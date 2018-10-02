1e8233cced0cc0190a73d4c41c45f7d98314dda9
Bedshaped <fraggle14@gmail.com>
2018-10-02 15:44:31 +0100

    fixing x to t
    more readable

--- radioactive_decay_euler.py
+++ radioactive_decay_euler.py
@@ -30,14 +30,14 @@ N_array = np.zeros((2, n))
 N_array[:,0] = 10
 
 # Loop our Euler method
-x = np.linspace(0, t_max, n)
-for t in range(n-1):
-    N_array[0, t + 1] = N_array[0, t] + d_t*(derivative(N_array[0, t], tau))
+t = np.linspace(0, t_max, n)
+for x in range(n-1):
+    N_array[0, x + 1] = N_array[0, x] + d_t*(derivative(N_array[0, x], tau))
 
     
 # Calculate direct values
 
-N_array[1] = N(x, N_array[1, 0], tau)
+N_array[1] = N(t, N_array[1, 0], tau)
 
 # Deviation of methods
     
@@ -46,14 +46,14 @@ dev = (N_array[1,:] - N_array[0,:])/N_array[1,:]
 # Initialize the plot
     
 plt.figure(1, figsize=(9, 6))
-plt.plot(N_array[0,:], x, label=r"$Euler (\Delta t = \frac{1}{%d}s)$" % d_t**-1)
-plt.plot(N_array[1,:], x, label=r"$Direct$")
+plt.plot(N_array[0,:], t, label=r"$Euler (\Delta t = \frac{1}{%d}s)$" % d_t**-1)
+plt.plot(N_array[1,:], t, label=r"$Direct$")
 plt.xlabel("Time")
 plt.ylabel("N")
 plt.legend()
 
 plt.figure(2, figsize=(9, 6))
-plt.plot(x[1:], dev[1:], label=r"$Deviation$")
+plt.plot(t[1:], dev[1:], label=r"$Deviation$")
 plt.yscale("log")
 plt.xscale("log")
 plt.xlabel("Time")
